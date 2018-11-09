cfc6895d76f9c912fb6801b54a58a6029d6475ea
calvin ho <calvinho1214@gmail.com>
2018-11-09 11:40:25 -0500

    third edit

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const layout = require('./views/layout');
 // const router = app.Router();
 
 app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 app.use('/', (req, res, next) => {
-  res.send('Hello World!!');
+  //res.send('Hello World!!');
+  res.send(layout(''));
 });
 
-const PORT = 1337;
-app.listen(1337, () => {
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
   console.log(`Listening on port , ${PORT}`);
 });
