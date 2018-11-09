465888778fb23c9322a802ad24720f30a20fb7f0
Frank Rose <franklin.rose@gmail.com>
2018-11-09 12:03:21 -0500

    Added User and Page db models

--- app.js
+++ app.js
@@ -2,18 +2,30 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const layout = require('./views/layout');
+const { db } = require('./models/index');
+const { Page } = require('./models/index');
+const { User } = require('./models/index');
 // const router = app.Router();
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use('/', (req, res, next) => {
   //res.send('Hello World!!');
-  res.send(layout(''));
+  res.send(layout('Hello World!!'));
 });
 
+const init = async () => {
+  await db.authenticate().then(() => {
+    console.log('connected to the database');
+  });
 
+  await Page.sync();
+  await User.sync();
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Listening on port , ${PORT}`);
-});
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+  });
+};
+
+init();
