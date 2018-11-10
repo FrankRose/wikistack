6cd2067a79d95b8cbf9a5d2be2f2975fede4da50
Frank Rose <franklin.rose@gmail.com>
2018-11-09 20:14:52 -0500

    Completed workshop

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const { Page, User } = require('../models');
+const userListPage = require('../views/userList');
+const userPage = require('../views/userPages');
 
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: { id: req.params.id },
+    });
+
+    const pages = await Page.findAll({
+      where: { authorId: user.id },
+    });
+
+    res.send(userPage(user, pages));
+  } catch (error) {
+    console.log(`Error retrieving user with id: ${req.params.id}`);
+  }
+});
+
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+
+    res.send(userListPage(users));
+  } catch (error) {
+    console.log('Error retrieving list of users', error);
+    next(error);
+  }
+});
 
 module.exports = router;
--- routes/wiki.js
+++ routes/wiki.js
@@ -20,60 +20,53 @@ router.get('/:slug', async (req, res, next) => {
       where: { id: requestedPage.authorId },
     });
     res.send(wikiPage(requestedPage, user));
-    // console.log(requestedPage);
   } catch (error) {
+    console.log('Error retrieving page', error);
     next(error);
   }
 });
 
 router.get('/', async (req, res, next) => {
-  // res.send('got to GET /wiki/');
-  // res.send(layout('You are on the Wiki page!!'));
   const allPages = await Page.findAll();
-  // console.log(allPages);
+
   res.send(mainPage(allPages));
 });
 
 router.post('/', async (req, res, next) => {
-  // console.log(req.body)
   const name = req.body.name;
   const email = req.body.email;
+  let user;
+
   let title = req.body.title;
   title = title.trim();
   const content = req.body.content;
   const status = req.body.status;
   let slug = '';
-
-  const user = new User({
-    name,
-    email,
-  });
+  let page;
 
   try {
-    await user.save();
+    [user] = await User.findOrCreate({
+      where: { name, email },
+    });
   } catch (error) {
-    console.log(error);
+    console.log('Error creating user', error);
     next(error);
   }
 
-  const page = new Page({
-    title,
-    content,
-    status,
-    slug,
-    authorId: user.id,
-  });
   try {
-    // await Page.create({
-    //   title,
-    //   content,
-    //   status,
-    //   slug,
-    // });
+    page = new Page({
+      title,
+      content,
+      status,
+      slug,
+      // authorId: user.id,
+    });
     await page.save();
-    // res.redirect(`/wiki/${title.replace(/\s+/g, '_').replace(/\W/g, '')}`);
+    page.setAuthor(user);
+
     res.redirect(`/wiki/${page.slug}`);
   } catch (error) {
+    console.log('Error saving page', error);
     next(error);
   }
 });
